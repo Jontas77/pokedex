328d36abed6b808c364a63906b9a9ae314a4245f
Jontas77 <mrjbheugh@gmail.com>
2021-05-15 14:28:17 +0200

    Completed Exercises A to I

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
-function App() {
+// Import Components
+import Logo from "./components/Logo";
+import BestPokemon from "./components/BestPokemon";
+import CaughtPokemon from "./components/CaughtPokemon";
+
+const App = () => {
+  const abilities = ["Anticipation", "Adaptability", "Run-Away"];
+  const date = new Date().toLocaleDateString();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Logo appName="Jonathan's Pokedex"/>
+      <BestPokemon abilities={abilities}/>
+      <CaughtPokemon date={date}/>
     </div>
   );
-}
+};
 
 export default App;
+
