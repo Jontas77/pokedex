55b00799933925cbefe293cd7d3941b37cc78d13
Jontas77 <mrjbheugh@gmail.com>
2021-05-18 22:43:56 +0200

    First commit

---
+++ src/components/BestPokemon.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+const BestPokemon = (props) => {
+    return (
+        <div>
+            <p>
+                My favourite Pokemon is Pikachu
+            </p>
+            <ul>
+                {props.abilities.map((ability, index) => (
+                    <li key={index}>{ability}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default BestPokemon;
+
---
+++ src/components/CaughtPokemon.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CaughtPokemon = (props) => (
+  <p>Caught 0 Pokemon on {props.date}</p>
+);
+
+export default CaughtPokemon;
